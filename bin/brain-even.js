d9b19f8edfaea1465aa10dc26f53fedebfe594c6
markkuchkudan <marknewlife1@gmail.com>
2021-12-12 19:14:40 +0200

    add 1st game

---
+++ bin/brain-even.js
@@ -0,0 +1,4 @@
+#!/usr/bin/env node
+import whi from '../src/kli.js';
+
+whi()
--- src/cli.js
+++ src/cli.js
@@ -1,8 +1,12 @@
 import readlineSync from 'readline-sync';
+var userName;
 
 const welcomingFunc = () => {
   console.log('Welcome to the Brain Games!');
-  const userName = readlineSync.question('May I have your name?');
-  console.log(`Hello ${userName}!`);
+  userName = readlineSync.question('May I have your name?');
+  console.log ('Hello ' + userName + '!');
 };
-export { welcomingFunc };
+export default welcomingFunc; 
+
+export { userName };
+
