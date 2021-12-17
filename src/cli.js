6a215ea2244b162649ec387301ffbc257e498f56
markkuchkudan <marknewlife1@gmail.com>
2021-12-17 14:55:59 +0200

    fix eslint calls

--- src/cli.js
+++ src/cli.js
@@ -4,5 +4,5 @@ function welcomingFunc() {
   console.log('Welcome to the Brain Games!');
   const userName = readlineSync.question('May I have your name?');
   console.log(`Hello ${userName}!`);
-};
+}
 export default welcomingFunc;
