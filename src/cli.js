62af5ee5bb231cbed7c62b42bdf62a68a86e7bcb
markkuchkudan <marknewlife1@gmail.com>
2022-01-04 20:23:00 +0200

    fix cli.js

--- src/cli.js
+++ src/cli.js
@@ -2,6 +2,6 @@ import readlineSync from 'readline-sync';
 
 function welcomingFunc() {
   const userName = readlineSync.question('May I have your name?');
-  console.log(`Hello ${userName}!`);
+  console.log(`Hello, ${userName}`);
 }
 export default welcomingFunc;
